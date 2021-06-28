aecd227f3a603fa6020a34d787fb27288e5f238a
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-06-28 20:36:36 -0300

    JSON creation 2/2

--- final_page/assets/js/modal.js
+++ final_page/assets/js/modal.js
@@ -1,3 +1,6 @@
+// Temporary JSON
+const json = '{ "guns": [ { "id": 0, "name": "TEST", "photo": "portfolio-0", "type": "none" }, { "id": 1, "name": "AK 47", "photo": "portfolio-1", "type": "auto" }, { "id": 2, "name": "M4", "photo": "portfolio-2", "type": "auto" }, { "id": 3, "name": "M 60", "photo": "portfolio-3", "type": "auto" }, { "id": 4, "name": "M1911", "photo": "portfolio-4", "type": "semi" }, { "id": 5, "name": "Magnum 44", "photo": "portfolio-5", "type": "semi" }, { "id": 6, "name": "M1 Garand", "photo": "portfolio-6", "type": "semi" }, { "id": 7, "name": "Breech Loader", "photo": "portfolio-7", "type": "manual" }, { "id": 8, "name": "Breech Loader 2", "photo": "portfolio-8", "type": "manual" }, { "id": 9, "name": "Kar 98k", "photo": "portfolio-9", "type": "manual" } ] }';
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -13,19 +16,26 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal with the correct information by checking id of the photo clicked
 function showModal(gun_id) {
+  var data = JSON.parse(json);
+  console.log(data);
+
+  // Need to change each gunImage1 (just change src), gunImage2 (just change src), gunInfo (change whole HTML), soundInfo (just change src)...
+  document.getElementById("gunImage1").src = 'assets/img/portfolio/'+data.guns[gun_id].photo+'.jpg';
+  document.getElementById("gunImage2").src = 'assets/img/portfolio/'+data.guns[gun_id].photo+'.jpg';
+
   console.log(gun_id);
   modal.style.display = "block";
 
-  var requestURL = '../data/data.json';
-  var request = new XMLHttpRequest();
-  request.open('GET', requestURL);
-  request.responseType = 'json';
-  request.send();
-  request.onload = function() {
-    var json = request.response;
-    var data = JSON.parse(json);
-    console.log(JSON.stringify(data));
-  }
+  // var requestURL = 'https://github.com/l-a-motta/online-store/blob/main/final_page/assets/data/data.json';
+  // var request = new XMLHttpRequest();
+  // request.open('GET', requestURL);
+  // request.responseType = 'json';
+  // request.send();
+  // request.onload = function() {
+  //   var json = request.response;
+  //   var data = JSON.parse(json);
+  //   console.log(JSON.stringify(data));
+  // }
 }
 
 // When the user clicks on <span> (x), close the modal
