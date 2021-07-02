19ab8fae27b695a741b7c5f6e631c93e19f4562a
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-02 20:07:09 -0300

    change portfolio for dynamic creation through javascript

--- final_page/assets/js/portfolio.js
+++
@@ -1,9 +0,0 @@
-// Temporary JSON
-const json = '{ "guns": [ { "id": 0, "name": "TEST", "photo": "portfolio-0", "type": "none" }, { "id": 1, "name": "M60", "photo": "portfolio-1", "type": "auto" }, { "id": 2, "name": "Old Musket", "photo": "portfolio-2", "type": "manual" }, { "id": 3, "name": "SP2022", "photo": "portfolio-3", "type": "semi" }, { "id": 4, "name": "M4", "photo": "portfolio-4", "type": "auto" }, { "id": 5, "name": "Magnum 44", "photo": "portfolio-5", "type": "semi" }, { "id": 6, "name": "M4A1", "photo": "portfolio-6", "type": "auto" }, { "id": 7, "name": "MP5", "photo": "portfolio-7", "type": "auto" }, { "id": 8, "name": "STNGR", "photo": "portfolio-8", "type": "auto" }, { "id": 9, "name": "AK 47", "photo": "portfolio-9", "type": "auto" } ] }';
-
-$(document).ready(function(){
-
-    // jQuery methods go here...
-    
-  
-  }); 
