f9f9f9053a47b3bdc14234394c81ccbc6148dfd3
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-02 19:05:58 -0300

    rename js file and delete old files

--- final_page/assets/js/modal.js
+++ final_page/assets/js/modal.js
@@ -7,12 +7,6 @@ var modal = document.getElementById("myModal");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// OBSOLETE WAY OF DOING MODAL
-// Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
 
 // When the user clicks on the button, open the modal with the correct information by checking id of the photo clicked
 function showModal(gun_id) {
@@ -25,17 +19,6 @@ function showModal(gun_id) {
 
   console.log(gun_id);
   modal.style.display = "block";
-
-  // var requestURL = 'https://github.com/l-a-motta/online-store/blob/main/final_page/assets/data/data.json';
-  // var request = new XMLHttpRequest();
-  // request.open('GET', requestURL);
-  // request.responseType = 'json';
-  // request.send();
-  // request.onload = function() {
-  //   var json = request.response;
-  //   var data = JSON.parse(json);
-  //   console.log(JSON.stringify(data));
-  // }
 }
 
 // When the user clicks on <span> (x), close the modal
