ca26e7bce70a17c52fcc49a9eff008838aa923a4
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-02 22:41:34 -0300

    finish design of shopping cart, still non-functional

--- final_page/assets/js/cart.js
+++ final_page/assets/js/cart.js
@@ -5,15 +5,19 @@ var cart = document.getElementById("myCart");
 var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the cart with the correct information by checking id of the photo clicked
-function showCart(gun_id) {
-  //document.getElementById("cart").innerHTML = 'HELLO id:' +gun_id;
-  cart.style.display = "block";
+function showCart() {
+    cart.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the cart modal
-span.onclick = function() {
+span.onclick = function () {
     cart.style.display = "none";
 }
 
 // Get JSON
 var data = JSON.parse(json);
+
+function addToCart(gun_id) {
+    //document.getElementById("cart").innerHTML = 'HELLO id:' +gun_id;
+    cart.style.display = "block";
+}
