065585e111334202adfe9076fcae69d72f638f74
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-02 23:05:14 -0300

    added dinamic cart functionality through javascript

--- final_page/assets/js/cart.js
+++ final_page/assets/js/cart.js
@@ -17,7 +17,30 @@ span.onclick = function () {
 // Get JSON
 var data = JSON.parse(json);
 
-function addToCart(gun_id) {
-    //document.getElementById("cart").innerHTML = 'HELLO id:' +gun_id;
-    cart.style.display = "block";
+function addCart() {
+    var gun_id = document.getElementById("gunId").innerHTML;
+    jQuery.each(data.guns, function (key, val) {
+
+        // console.log('val.id: '+val.id);
+        // console.log('gun_id: '+gun_id);
+        if (val.id == gun_id) {
+            $("#cartList").append(
+            '<tr id="gun-'+val.id+'">' +
+                '<th scope = "row" class= "border-0" >' +
+                '<div class="p-2">' +
+                    '<img src="assets/img/portfolio/'+val.photo+'.jpg" alt="" class="img-fluid" width="100px" style="padding-right:15px;">' +
+                    '<div class="ml-3 d-inline-block align-middle">' +
+                    '<h5 class="mb-0"> <a class="text-dark d-inline-block align-middle">'+val.name+'</a> </h5>' +
+                    '<span class="text-muted font-weight-normal font-italic d-block">'+val.type+'</span>' +
+                    '</div>' +
+                '</div>' +
+                '</th>' +
+                '<td class="border-0 align-middle"><strong>$79.00</strong></td>' +
+                '<td class="border-0 align-middle"><strong>1</strong></td>' +
+                '<td class="border-0 align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>' +
+            '</tr>');
+            return false;
+        }
+
+    });
 }
--- final_page/assets/js/modal.js
+++ final_page/assets/js/modal.js
@@ -8,14 +8,13 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal with the correct information by checking id of the photo clicked
 function showModal(gun_id) {
   var data = JSON.parse(json);
-  console.log(data);
 
   // Need to change each gunImage1 (just change src), gunImage2 (just change src), gunInfo (change whole HTML), soundInfo (just change src)...
   document.getElementById("gunImage1").src = 'assets/img/portfolio/'+data.guns[gun_id].photo+'.jpg';
   document.getElementById("gunImage2").src = 'assets/img/portfolio/'+data.guns[gun_id].photo+'.jpg';
-  document.getElementById("gunName").innerHTML = data.guns[gun_id].id +' - '+data.guns[gun_id].name;
+  document.getElementById("gunName").innerHTML = data.guns[gun_id].name;
+  document.getElementById("gunId").innerHTML = data.guns[gun_id].id;
 
-  console.log(gun_id);
   modal.style.display = "block";
 }
 
