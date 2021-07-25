a447b9fee406d11aa3faa110a48287b934dc9ef7
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-25 10:54:14 -0300

    post catching for addgun

--- online-store/routes/guns.js
+++ online-store/routes/guns.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET guns listing. */
-router.get('/gunlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-      res.json(docs);
-    });
+router.get('/gunlist', function (req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function (e, docs) {
+    res.json(docs);
   });
+});
+
+/* POST to addgun. */
+router.post('/addgun', function (req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.insert(req.body, function (err, result) {
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
+});
 
 module.exports = router;
