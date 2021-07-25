caa695f7d351ff0ada03cfeab0d0b8ed1864751a
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-24 21:30:47 -0300

    adjustment so modal works with new ids

--- online-store/public/js/modal.js
+++ online-store/public/js/modal.js
@@ -12,8 +12,11 @@ function showModal(gun_id) {
   // Need to change each gunImage1 (just change src), gunImage2 (just change src), gunInfo (change whole HTML), soundInfo (just change src)...
   document.getElementById("gunImage1").src = '/img/portfolio/'+data.guns[gun_id].photo+'.jpg';
   document.getElementById("gunImage2").src = '/img/portfolio/'+data.guns[gun_id].photo+'.jpg';
+  document.getElementById("gunNumber").innerHTML = data.guns[gun_id].id;
   document.getElementById("gunName").innerHTML = data.guns[gun_id].name;
-  document.getElementById("gunId").innerHTML = data.guns[gun_id].id;
+  document.getElementById("gunType").innerHTML = data.guns[gun_id].id;
+  document.getElementById("gunPrice").innerHTML = data.guns[gun_id].id;
+  document.getElementById("gunDescription").innerHTML = data.guns[gun_id].id;
 
   modal.style.display = "block";
 }
