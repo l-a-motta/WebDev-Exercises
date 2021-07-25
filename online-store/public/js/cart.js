ed46455a4c996a8a5bfca06a1af815a02cc61c64
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-25 17:08:29 -0300

    cart now adds proper guns

--- online-store/public/js/cart.js
+++ online-store/public/js/cart.js
@@ -20,39 +20,33 @@ var data = JSON.parse(json);
 function addCart() {
     document.getElementById("myModal").style.display = "none";
 
-    var gun_id = document.getElementById("gunNumber").innerHTML;
+    var gun_id = document.getElementById("gunModalId").innerHTML;
 
     // Get Index of object based on id value
-    var arrayPosition = gunListData.map(function(arrayItem) { return arrayItem.number; }).indexOf(gun_id);
+    var arrayPosition = gunListData.map(function (arrayItem) { return arrayItem._id; }).indexOf(gun_id);
 
-    // Get our User Object
+    // Get our gun Object
     var thisGunObject = gunListData[arrayPosition];
 
     console.log(thisGunObject);// Will do cart things later with this
 
-    jQuery.each(data.guns, function (key, val) {
+    cartContent = '<tr id="gun-' + thisGunObject._id + '">' +
+    '<th scope = "row" class= "border-0" >' +
+    '<div class="p-2">' +
+    '<img src="/img/portfolio/portfolio-' + thisGunObject.number + '.jpg" alt="" class="img-fluid" width="100px" style="padding-right:15px;">' +
+    '<div class="ml-3 d-inline-block align-middle">' +
+    '<h5 class="mb-0"> <a class="text-dark d-inline-block align-middle">' + thisGunObject.name + '</a> </h5>' +
+    '<span class="text-muted font-weight-normal font-italic d-block">' + thisGunObject.type + '</span>' +
+    '</div>' +
+    '</div>' +
+    '</th>' +
+    '<td class="border-0 align-middle"><strong>$' + thisGunObject.price + '.00</strong></td>' +
+    '<td class="border-0 align-middle"><strong>1</strong></td>' +
+    '<td class="border-0 align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>' +
+    '</tr>'
 
-        // console.log('val.id: '+val.id);
-        // console.log('gun_id: '+gun_id);
-        if (val.id == gun_id) {
-            $("#cartList").append(
-            '<tr id="gun-'+val.number+'">' +
-                '<th scope = "row" class= "border-0" >' +
-                '<div class="p-2">' +
-                    '<img src="/img/portfolio/portfolio-'+val.number+'.jpg" alt="" class="img-fluid" width="100px" style="padding-right:15px;">' +
-                    '<div class="ml-3 d-inline-block align-middle">' +
-                    '<h5 class="mb-0"> <a class="text-dark d-inline-block align-middle">'+val.name+'</a> </h5>' +
-                    '<span class="text-muted font-weight-normal font-italic d-block">'+val.type+'</span>' +
-                    '</div>' +
-                '</div>' +
-                '</th>' +
-                '<td class="border-0 align-middle"><strong>$'+val.price+'.00</strong></td>' +
-                '<td class="border-0 align-middle"><strong>1</strong></td>' +
-                '<td class="border-0 align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>' +
-            '</tr>');
-            return false;
-        }
+    // Inject the whole content string into our existing HTML table
+    $('#cartList table tbody').append(cartContent);
 
-    });
     showCart();
 }
